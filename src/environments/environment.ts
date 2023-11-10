991f57a3a99c028e2b2a8d91ea20dd6a37066aad
Juan Carlos Reyes <ingreyesguerrero@gmail.com>
2023-11-10 08:33:22 -0500

    Arreglo traducciones
    Arreglo traducciones

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   url: '',
-  //endpoint:"https://localhost:7207/api/"
-  endpoint: "https://dommunbackend.azurewebsites.net/api/",
+  endpoint:"https://localhost:7207/api/",
+  //endpoint: "https://dommunbackend.azurewebsites.net/api/",
   credentialsKey: 'Yh2k7QSu4l8CZg5p6X3Pna9L0Miy4D3Bvt0JVr87UcOj69Kqw5R2Nmf4FWs03Hdx',
   credentialsApiKey: 'Yb45&AcJAj4D5n@zE3KF0N%CJF5Owjk0s1*uL%gX!3L2qCT2xw'
 };
